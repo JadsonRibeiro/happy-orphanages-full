54e45ec2e3bb8fb695827d7a7c2dc51ce808f44e
Jadson Ribeiro (Notebook) <jdsntj@gmail.com>
2020-10-16 16:50:29 -0300

    Concluded Happy 1.0

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useFonts } from 'expo-font';
-import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold_Italic } from '@expo-google-fonts/nunito';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold_Italic, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
 
 import Routes from './src/routes';
 
@@ -8,7 +8,8 @@ export default function App() {
   const [fontLoaded] = useFonts({
     Nunito_600SemiBold,
     Nunito_700Bold,
-    Nunito_800ExtraBold_Italic
+    Nunito_800ExtraBold_Italic,
+    Nunito_800ExtraBold
   });
 
   if (!fontLoaded) {
